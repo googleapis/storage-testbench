eda2d5df7047fab7ca4ca59a5a9ba647b5fa623b
Carlos O'Ryan <coryan@google.com>
2021-07-30 01:52:18 +0000

    Remove unused code

--- testbench/common.py
+++ testbench/common.py
@@ -14,22 +14,9 @@
 
 """Common utils"""
 
-import base64
-import json
-import random
 import re
 import types
-import sys
-import socket
-import struct
 from flask import Response as FlaskResponse
-
-import scalpl
-import testbench
-
-from google.protobuf import timestamp_pb2
-from requests_toolbelt import MultipartDecoder
-from requests_toolbelt.multipart.decoder import ImproperBodyPartContentException
 
 re_remove_index = re.compile(r"\[\d+\]+|^[0-9]+")
 retry_return_error_code = re.compile(r"return-([0-9]+)$")
@@ -45,10 +32,6 @@ re_snake_case = re.compile(r"(?<!^)(?=[A-Z])")
 
 def to_snake_case(string):
     return re_snake_case.sub("_", string).lower()
-
-
-def remove_index(string):
-    return re_remove_index.sub("", string)
 
 
 # === FAKE REQUEST === #
@@ -76,69 +59,3 @@ class FakeRequest(types.SimpleNamespace):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-    @classmethod
-    def init_xml(cls, request):
-        headers = {
-            key.lower(): value
-            for key, value in request.headers.items()
-            if key.lower().startswith("x-goog-") or key.lower() == "range"
-        }
-        args = request.args.to_dict()
-        args.update(cls.xml_headers_to_json_args(headers))
-        return cls(args=args, headers=headers)
-
-    @classmethod
-    def xml_headers_to_json_args(cls, headers):
-        field_map = {
-            "x-goog-if-generation-match": "ifGenerationMatch",
-            "x-goog-if-metageneration-match": "ifMetagenerationMatch",
-            "x-goog-acl": "predefinedAcl",
-        }
-        args = {}
-        for field_xml, field_json in field_map.items():
-            if field_xml in headers:
-                args[field_json] = headers[field_xml]
-        return args
-
-    def HasField(self, field):
-        return hasattr(self, field) and getattr(self, field) is not None
-
-    @classmethod
-    def init_protobuf(cls, request, context):
-        fake_request = FakeRequest(args={}, headers={})
-        fake_request.update_protobuf(request, context)
-        return fake_request
-
-    def update_protobuf(self, request, context):
-        for (
-            proto_field,
-            args_field,
-        ) in FakeRequest.protobuf_wrapper_to_json_args.items():
-            if hasattr(request, proto_field) and request.HasField(proto_field):
-                self.args[args_field] = getattr(request, proto_field).value
-                setattr(self, proto_field, getattr(request, proto_field))
-        for (
-            proto_field,
-            args_field,
-        ) in FakeRequest.protobuf_scalar_to_json_args.items():
-            if hasattr(request, proto_field):
-                self.args[args_field] = getattr(request, proto_field)
-                setattr(self, proto_field, getattr(request, proto_field))
-        csek_field = "common_object_request_params"
-        if hasattr(request, csek_field):
-            algorithm, key_b64, key_sha256_b64 = testbench.csek.extract(
-                request, False, context
-            )
-            self.headers["x-goog-encryption-algorithm"] = algorithm
-            self.headers["x-goog-encryption-key"] = key_b64
-            self.headers["x-goog-encryption-key-sha256"] = key_sha256_b64
-            setattr(self, csek_field, getattr(request, csek_field))
-        elif not hasattr(self, csek_field):
-            setattr(
-                self,
-                csek_field,
-                types.SimpleNamespace(
-                    encryption_algorithm="", encryption_key="", encryption_key_sha256=""
-                ),
-            )
