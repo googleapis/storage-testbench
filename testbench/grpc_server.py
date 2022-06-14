e295d5a11017282dd83b5f6bbf194304cf1333c5
Chris Cotter <cjcotter@google.com>
2022-06-14 11:31:18 -0400

    fix: gRPC logging check for iterable
    This line was causing failures previously when running tests
    in Go, but it works when the check uses collections.Iterable
    instead.

--- testbench/grpc_server.py
+++ testbench/grpc_server.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import base64
+import collections
 from concurrent import futures
 import functools
 import datetime
@@ -70,7 +71,7 @@ def _logging_method_decorator(function):
 
     @functools.wraps(function)
     def decorated(self, request, context):
-        if isinstance(request, (types.GeneratorType, list)):
+        if isinstance(request, (types.GeneratorType, collections.Iterable)):
             id = uuid.uuid4().hex
             input = "in[" + id + "]"
             request = _format_input_generator(function.__name__, id, request)
