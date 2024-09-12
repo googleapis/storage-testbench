095846b93875ee81e38dab232d7d7ec8956b3bde
Denis DelGrosso <ddelgrosso@google.com>
2024-09-12 16:10:36 +0000

    Revert "update protobuf to 5.27.2"
    This reverts commit 7e1d116248429125bbf321880db577f0d9195ca7.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setuptools.setup(
     install_requires=[
         "grpcio==1.66.1",
         "googleapis-common-protos==1.65.0",
-        "protobuf==5.27.2",
+        "protobuf==4.25.4",
         "flask==3.0.3",
         "requests-toolbelt==1.0.0",
         "scalpl==0.4.2",
