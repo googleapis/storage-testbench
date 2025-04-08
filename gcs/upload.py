b54d574f7f9fabc96e024b86eb36d765bf753ba2
Cathy Ouyang <cathyo@google.com>
2025-04-08 10:55:24 -0700

    feat(example): update appendable blob status in unfinalized writes

--- gcs/upload.py
+++ gcs/upload.py
@@ -505,6 +505,23 @@ class Upload(types.SimpleNamespace):
                 # instead of the 15 seconds interval used in the GCS server.
                 # TODO(#592): Refactor testbench checkpointing to more closely follow GCS server behavior.
                 upload.media += content
+
+                # Update appendable blob size and media here, as an example.
+                # TODO: (1) Update_time, size and crc are updated in object metadata.
+                # TODO: (2) Whether the testbench checks for flush or/and performs a background force-close.
+                if is_appendable:
+                    def update_appendable_blob(blob, unused_generation):
+                        blob.media = upload.media
+                        blob.metadata.size = len(upload.media)
+                        return blob
+
+                    blob = db.do_update_object(
+                        upload.bucket.name,
+                        upload.metadata.name,
+                        update_fn=update_appendable_blob,
+                        context=context,
+                        generation=upload.metadata.generation,
+                    )
             elif data is not None:
                 testbench.error.invalid("Invalid data field in upload", context)
 
